92f76fa096de9a1d8e9205a587474fcf531359dc
luis leiva <lileiva@uc.cl>
2020-06-20 04:57:11 -0400

    add button to homescreen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { Home } from "../Features/Home";
 import { Page } from "../Components/Page";
+import { ModalCalendar } from "../Components/Modal";
 
 export default () => {
   return (
     <Page>
+      <ModalCalendar/>
       <Home title="30 días de react - “Estilando nuestros pokemones con CSS in JS”" />
     </Page>
   );
