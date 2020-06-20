6ec7e761a61e804b5dee193bab7c4b220a57e551
luis leiva <lileiva@uc.cl>
2020-06-20 05:45:15 -0400

    new transition of close, and new disposition of components

--- Components/Modal/calendar.tsx
+++ Components/Modal/calendar.tsx
@@ -10,11 +10,6 @@ type KeyPress = {
 }
 
 /**
- * @description get the position of the circle
- * @param {Boolean} active is the modal active?
- */
-const position = (active: Boolean) => active? `-150vmax`: "-66px"
-/**
  * @description get the diameter of the circle
  * @param {Boolean} active is the modal active?
  */
@@ -24,8 +19,9 @@ const diameter = (active: Boolean) => active? `300vmax`: "132px"
 const StyledButton = styled.div<CalendarProps>`
   position: fixed;
   overflow: hidden;
-  right: ${(props) => position(props.active)}; 
-  top: ${(props) => position(props.active)}; 
+  right: 0px; 
+  top: 0px; 
+  transform: translate(50%, -50%);
   bottom: 0%;  
   border-radius: 50%;
   width: ${(props) => diameter(props.active)}; 
@@ -38,11 +34,12 @@ const StyledButton = styled.div<CalendarProps>`
 /** @description Style for the close button (it is a div because buttons have to many styles) */
 const StyledClose = styled.div<CalendarProps>`
   position: fixed;
-  width: ${(props) => props.active? '25px': '0px'};
-  height: ${(props) => props.active? '25px': '0px'};
+  opacity: ${(props) => props.active? '1': '0'};
+  width: 25px;
+  height: 25px;
   right: 10px;
   top: 10px;
-  border: ${(props) => props.active? '2px solid': '0px'};
+  border: 2px solid;
   border-color: #fff;
   cursor: pointer;
   border-radius: 50%;
@@ -51,7 +48,7 @@ const StyledClose = styled.div<CalendarProps>`
   vertical-align: middle;
   & span { 
     transition: 2s;
-    font-size: ${(props) => props.active? '1em;': '0px'};
+    font-size: 1em;
     color: #FFF;
     margin: auto;
     text-align: center;
@@ -71,15 +68,16 @@ const Content = styled.div<CalendarProps>`
 /** @description style for de icon this section have some delay in transitions */
 const IconContainer = styled.div<CalendarProps>`
   position: fixed;
-  width: ${(props) => props.active? '25vw;': '30px'};
+  width: ${(props) => props.active? '25vmin;': '30px'};
   top: ${(props) => props.active? '50%;': '25px'};
   right: ${(props) => props.active? '50%;': '25px'};
   transform: translate(50%, -50%);
   opacity: ${(props) => props.active? '0': '1'};
   transition-delay: 5s;
+  cursor: pointer;
   transition:${(props) => props.active? '2s': '1.5s 0.5s'}; 
   & svg {
-    fill: ${(props) => props.active? '#AAA': '#FFF'};
+    fill: ${(props) => props.active? '#CCC': '#FFF'};
   }
 `
 /* content and iconContainer have delay in layout 
@@ -112,18 +110,21 @@ const ModalCalendar: React.FC = ({ children }) => {
       return document.removeEventListener("keydown", (e) =>log(e), false);
     }
   },[])
-  return <StyledButton onClick={() => {if(!active) open()}} active={active}>
-          <StyledClose onClick={() => close()} active={active} >
-            <span>X</span>
-          </StyledClose>
-          <IconContainer active={active}>
-            {/* this is the svg of the calendar */}
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M1 4c0-1.1.9-2 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4zm2 2v12h14V6H3zm2-6h2v2H5V0zm8 0h2v2h-2V0zM5 9h2v2H5V9zm0 4h2v2H5v-2zm4-4h2v2H9V9zm0 4h2v2H9v-2zm4-4h2v2h-2V9zm0 4h2v2h-2v-2z"/></svg>
-          </IconContainer>
-          <Content active={active} > 
-            { children }
-            {/* {[1,2,3,4].map((i) => <Line key={i}> hola </Line>)} */}
-          </Content>
-        </StyledButton>;
+  return (
+  <> 
+  <StyledButton onClick={() => {if(!active) open()}} active={active}>
+  </StyledButton>;
+  <StyledClose onClick={() => close()} active={active} >
+    <span>X</span>
+  </StyledClose>
+  <IconContainer active={active} onClick={() => {if(!active) open()}}>
+    {/* this is the svg of the calendar */}
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M1 4c0-1.1.9-2 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4zm2 2v12h14V6H3zm2-6h2v2H5V0zm8 0h2v2h-2V0zM5 9h2v2H5V9zm0 4h2v2H5v-2zm4-4h2v2H9V9zm0 4h2v2H9v-2zm4-4h2v2h-2V9zm0 4h2v2h-2v-2z"/></svg>
+  </IconContainer>
+  <Content active={active} > 
+    { children }
+    {/* {[1,2,3,4].map((i) => <Line key={i}> hola </Line>)} */}
+  </Content>
+  </>)
 };
 export default ModalCalendar
